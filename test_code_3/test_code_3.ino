3b0dea7f3b8efedb721fcda101cd21ac4b462546
tyiu-VAIO\t yiu <kendotwins@gmail.com>
2017-08-30 13:31:15 +1000

    Basic Ohm's Law Demo Code as from the lecture notes "6 How to build and autonomous robot PART 1 of 3.docx" page 9

--- test_code_3/test_code_3.ino
+++ test_code_3/test_code_3.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+// This program generates random values for Voltage and Current calculates 
+// and calculates the current in a circuit
 
+float Current = 4.7;    //Dummy initial value in Amperes
+float Voltage = 12;   // Dummy initial value in Volts
+float Resistance = 56;  // Dummy initial value in Ohms
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10000);
+  randomSeed(analogRead(1));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Voltage = random(10,300)/10;
+  Resistance = random(2,400);
+  Serial.print("V = ");
+  Serial.println(Voltage);
+  Serial.print("R = ");
+  Serial.println(Resistance);
+  Serial.print("Current (I) = ");
+  Serial.println(Voltage/Resistance, 3);
+  Serial.println();
+  delay(2000); 
 }
+
