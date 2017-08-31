e55a7f11b163894964633873838b1456be79c116
tyiu-VAIO\t yiu <kendotwins@gmail.com>
2017-08-31 11:43:03 +1000

    Test code successfully detect claps

--- test_code_3/test_code_3.ino
+++ test_code_3/test_code_3.ino
@@ -1,26 +1,51 @@
-// This program generates random values for Voltage and Current calculates 
-// and calculates the current in a circuit
+// This program sourced from
+// https://www.arduino.cc/en/Reference/AttachInterrupt
+// to test the interrupt pin 
+// NOTES: The gain of the audio sensor is tuned CCW to very low until the RED LED on the module is just OFF
 
-float Current = 4.7;    //Dummy initial value in Amperes
-float Voltage = 12;   // Dummy initial value in Volts
-float Resistance = 56;  // Dummy initial value in Ohms
+const byte ledPin = 5;
+const byte interruptPin = 2;
+int blinkCnt = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.setTimeout(10000);
-  randomSeed(analogRead(1));
+  Serial.println("setup() - begin");
+  pinMode(ledPin, OUTPUT);
+//  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED_OFF");
+  Serial.println("setup() - end");
 }
 
 void loop() {
-  Voltage = random(10,300)/10;
-  Resistance = random(2,400);
-  Serial.print("V = ");
-  Serial.println(Voltage);
-  Serial.print("R = ");
-  Serial.println(Resistance);
-  Serial.print("Current (I) = ");
-  Serial.println(Voltage/Resistance, 3);
-  Serial.println();
-  delay(2000); 
+//  state = !state;
+  if (blinkCnt)
+  {
+     blinkCnt = 0;
+     digitalWrite(ledPin, HIGH);
+     Serial.println("LED_ON");
+     delay(1000);
+  }
+  digitalWrite(ledPin, LOW);
+
+/*
+  if (digitalRead(interruptPin) == HIGH)
+  {
+    //digitalWrite(ledPin, HIGH);
+    analogWrite(ledPin, 50);
+  }
+  else
+  {
+    //digitalWrite(ledPin, LOW);
+    analogWrite(ledPin, 0);
+  }
+  delay(50);
+*/  
 }
 
+void blink() {
+  blinkCnt++;
+  Serial.println(millis());
+  //state = !state;
+}
